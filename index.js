bcfbd26fafc6268cb2615e2e86db471fbb2fa55d
Jose David <jose7312000@gmail.com>
2022-09-24 18:50:16 -0500

    Definiendo las primeras rutas

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-import express  from "express";
+import express from "express";
 const app = express();
 
 const port = 3000;
 
-app.listen(port, () => { 
+app.get("/empleados", (req, res) => res.send("retornando empleados"))
+
+app.post("/empleados", (req, res) => res.send("creando empleados"))
+
+app.put("/empleados", (req, res) => res.send("editando empleados"))
+
+app.delete("/empleados", (req, res) => res.send("eliminando empleados"))
+
+app.listen(port, () => {
     console.log("Corriendo sin problemas");
 })
