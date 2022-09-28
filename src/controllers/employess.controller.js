d8d9cfb3829f64b3ecb32e0d22c976ef1272f6de
Jose David <jose7312000@gmail.com>
2022-09-28 00:06:41 -0500

    Mostrando empleado por id

--- src/controllers/employess.controller.js
+++ src/controllers/employess.controller.js
@@ -1,17 +1,34 @@
 import { pool } from '../database/conexion.js'
 //Controladores que apuntan a una ruta
-export const getEmpleados =  async (req, res) =>{
+
+//Lista todos los empleados
+export const getEmpleados = async (req, res) => {
     const [rows] = await pool.query('SELECT * FROM EMPLOYEE')
     res.send(rows)
 }
 
+//Muestra la informacion de un empleado
+export const getEmpleado = async (req, res) => {
+    const { id } = req.params;
+    const [rows] = await pool.query("SELECT * FROM EMPLOYEE WHERE id = ?", [
+        id,
+    ]);
+
+    if (rows.length <= 0) {
+        return res.status(404).json({ message: "Employee not found" });
+    }
+    res.send(rows)
+
+}
+
+//Crea un nuevo empleado
 export const createEmpleados = async (req, res) => {
-    const {name, salary} = req.body //Accedemos a lo que se envia en el request
+    const { name, salary } = req.body //Accedemos a lo que se envia en el request
     //Con rows accedemos al metodo que devuleve lo que se inserto
-    const [rows] = await  pool.query('INSERT INTO employee (name, salary) VALUES (?, ?)', [name, salary])
+    const [rows] = await pool.query('INSERT INTO employee (name, salary) VALUES (?, ?)', [name, salary])
     res.send({
         id: rows.insertId,
-        name, 
+        name,
         salary
     })
 };
--- src/routes/employees.routes.js
+++ src/routes/employees.routes.js
@@ -1,8 +1,9 @@
 import {Router} from 'express';
-import { getEmpleados, createEmpleados, deleteEmpleados, editEmpleados } from '../controllers/employess.controller.js';
+import { getEmpleados, createEmpleados, deleteEmpleados, editEmpleados, getEmpleado } from '../controllers/employess.controller.js';
 const router = Router();
 
 router.get("/empleados", getEmpleados)
+router.get("/empleado/:id", getEmpleado)
 router.post("/empleados", createEmpleados)
 router.put("/empleados", editEmpleados)
 router.delete("/empleados", deleteEmpleados)
